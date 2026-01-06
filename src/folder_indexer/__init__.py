d4c567455367df1cd426d12d3ac29befe33ca1ff
RecRanger <168371178+RecRanger@users.noreply.github.com>
2026-01-05 18:48:20 -0700

    Release version 0.2.1

--- src/folder_indexer/__init__.py
+++ src/folder_indexer/__init__.py
@@ -1,5 +1,11 @@
-from .indexer import (  # noqa: F401
+from .indexer import (
     main_cli,
     merge_parquets,
     run_file_indexer,
 )
+
+__all__ = [
+    "main_cli",
+    "merge_parquets",
+    "run_file_indexer",
+]
